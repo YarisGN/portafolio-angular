efe8d7968be149614abaf33c1c2dae617c3728b7
YarisGN <yarisgomez255@gmail.com>
2025-02-27 17:27:12 -0500

    Fin sección 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Producto } from '../interfaces/producto.interface';
+import { Observable } from 'rxjs';
+import { ProductoDescripcion } from '../interfaces/producto.descripcion.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,7 @@ export class ProductosService {
 
   cargando= true;
   productos : Producto[] = [];
+  productosfiltrado: Producto[] = [];
 
   constructor( private http: HttpClient ) { 
 
@@ -18,19 +21,71 @@ export class ProductosService {
 
   // Para cargar productos
   private cargarProductos() {
-    this.http.get<Producto[]>('https://portafolio-angular-baf1a-default-rtdb.firebaseio.com/productos_idx.json')
-      .subscribe( (resp: Producto[]) => {
 
-        /* console.log(resp); */
-        this.productos = resp;
-        this.cargando = false;
+    return new Promise<void> ( ( resolve, reject) => {
 
-        /* setTimeout(() => {
+      this.http.get<Producto[]>('https://portafolio-angular-baf1a-default-rtdb.firebaseio.com/productos_idx.json')
+        .subscribe( (resp: Producto[]) => {
+  
+          /* console.log(resp); */
+          this.productos = resp;
           this.cargando = false;
-        }, 2000); */
+          resolve();
+  
+          /* setTimeout(() => {
+            this.cargando = false;
+          }, 2000); */
+  
+        });
 
-      })
+    });
+
   }
+
+  getProducto(id: string): Observable<ProductoDescripcion> {
+    return this.http.get<ProductoDescripcion>(`https://portafolio-angular-baf1a-default-rtdb.firebaseio.com/productos/${id}.json`);
+  }
+
+
+  buscarProducto( termino: string ) {
+   
+    if ( this.productos.length === 0 ) {
+      // cargar productos
+      this.cargarProductos().then( () => {
+        // ejecutar despues de tener los productos
+        // aplicar filtro
+        this.filtrarProductos( termino );
+      });
+
+    } else {
+      // aplicar el filtro
+      this.filtrarProductos( termino );
+    }
+
+    
+  }
+
+
+  private filtrarProductos( termino: string ) {
+
+    console.log(this.productos);
+    this.productosfiltrado = [];
+
+    //Para cambiarlo a minuscula
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+        this.productosfiltrado.push( prod );
+      }
+
+    })
+
+  }
+
 }
 
 
